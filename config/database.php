a6ef3fd4c694e87e12a333eb15b787deddc5e41e
AleydGarcia <aleyd.garcia.itid@unipolidgo.edu.mx>
2026-08-14 02:18:07 -0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -64,8 +64,7 @@ return [
                     (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
                 ]) : [],
                 [
-                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // Obligatorio para conexiones seguras en la nube como TiDB
-                ]
+                    \Pdo\Mysql::ATTR_SSL_VERIFY_SERVER_CERT => false, // Obligatorio para conexiones seguras en la nube como TiDB                ]
             ),
         ],
 
